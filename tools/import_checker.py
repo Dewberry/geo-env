76707de36ac8e40958ef3a0b709b696339e2552b
albrazeau <abrazeau1993@gmail.com>
2019-10-11 18:03:44 -0400

    Finalize a working version of batch_install.bat, update readme. batch_import tested on a clean cloud instance with success.

--- tools/import_checker.py
+++ tools/import_checker.py
@@ -1,16 +1,23 @@
 import subprocess
 
 if __name__ == '__main__':
-	result = subprocess.run(['pip', 'freeze'], stdout=subprocess.PIPE)
-	stringout = result.stdout.decode('utf-8')
-	modules = [x.split('==')[0] for x in stringout.split('\r\n')]
+    result = subprocess.run(['pip', 'freeze'], stdout=subprocess.PIPE)
+    stringout = result.stdout.decode('utf-8')
+    modules = [x.split('==')[0] for x in stringout.split('\r\n')]
 
-	with open('import_errors.txt' , 'w') as errors:
-		for module in modules:
-		    try:
-		        exec('import {}'.format(module.lower()))
-		    except ModuleNotFoundError as e:
-		        print(e)
-		        errors.write(str(e) + '\n')
-		    except SyntaxError as e:
-		        pass
+    with open('import_log.txt' , 'w') as log:
+        for module in modules:
+            try:
+                exec('import {}'.format(module.lower()))
+                log.write('Succesfully imported: {}\n'.format(module.lower()))
+            except ModuleNotFoundError as mnfe:
+                print(mnfe)
+                log.write('\n' + str(mnfe) + '\n\n')
+            except ImportError as ie:
+                print(ie)
+                log.write('\n' + str(ie) + '\n\n')
+            except OSError as ose:
+                print(ose)
+                log.write('\n' + str(ose) + '\n\n')
+            except SyntaxError as se:
+                pass 
